dc6e3a81af5085307dac23dbbb4a9d328ea64a50
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-11-15 22:47:42 -0300

    feat: create utils

---
+++ src/utils/utils.js
@@ -0,0 +1,46 @@
+import { toast } from "react-toastify";
+
+const today = new Date();
+
+export const getMonth = (month = today.getMonth()) => {
+  const year = today.getFullYear();
+  const firstDayOfTheMonth = new Date(year, month, 1).getDay();
+  let currentMonthCount = 0 - firstDayOfTheMonth;
+
+  return new Array(5).fill([]).map(() =>
+    new Array(7).fill(null).map(() => {
+      currentMonthCount++;
+      return new Date(year, month, currentMonthCount);
+    })
+  );
+};
+
+const toastMessageDefaults = {
+  position: "bottom-left",
+  autoClose: 3000,
+  hideProgressBar: true,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+};
+
+export const notifySuccess = (message) => {
+  toast.success(message, { ...toastMessageDefaults });
+};
+
+export const notifyError = (message) => {
+  toast.error(message, { ...toastMessageDefaults });
+};
+
+export const formatDate = (date) => {
+  const options = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  };
+  return date.toLocaleString("en-US", options);
+};
+
+export const colors = ["purple", "gray", "green", "red", "blue", "pink"];
