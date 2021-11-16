91e449ecd283e31529598fcb97fecb06d2d29489
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-11-15 22:46:15 -0300

    feat: create input component

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,91 @@
+import { forwardRef } from "react";
+import ReactTooltip from "react-tooltip";
+
+import warningImg from "../../assets/warning.svg";
+
+export const Input = forwardRef(
+  (
+    {
+      label,
+      type,
+      name,
+      placeholder,
+      value,
+      onChange,
+      onBlur,
+      error,
+      maxLength,
+      initialValue,
+    },
+    ref
+  ) => (
+    <div className={`field ${error ? "field__input-container--error" : ""}`}>
+      <label className="field__label">{label}</label>
+      {type === "text" && (
+        <>
+          <input
+            readOnly={value ? true : false}
+            maxLength={maxLength}
+            type={type}
+            name={name}
+            id={name}
+            className={`field__input ${error ? "field__input--error" : ""}`}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            onBlur={onBlur}
+            ref={ref}
+          />
+        </>
+      )}
+
+      {type === "textarea" && (
+        <textarea
+          readOnly={value ? true : false}
+          maxLength={maxLength}
+          name={name}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+          placeholder={placeholder}
+          ref={ref}
+          className={`field__textarea ${error ? "field__textarea--error" : ""}`}
+        />
+      )}
+
+      {type === "datetime-local" && (
+        <input
+          // readOnly={value ? true : false}
+          type={type}
+          maxLength={maxLength}
+          name={name}
+          value={value}
+          initialValue={initialValue}
+          onChange={onChange}
+          onBlur={onBlur}
+          placeholder={placeholder}
+          ref={ref}
+          className={`field__input ${error ? "field__input--error" : ""}`}
+        />
+      )}
+
+      {error && (
+        <>
+          <img
+            className="field__error-icon"
+            data-tip={error}
+            src={warningImg}
+            alt="Warning"
+          />
+          <ReactTooltip
+            textColor="#fff"
+            backgroundColor="#f95e5a"
+            type="error"
+            effect="solid"
+            place="top"
+          />
+        </>
+      )}
+    </div>
+  )
+);
